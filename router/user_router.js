3a3aecf819b98ef2c0b1401dc25ff72f3b624f2f
Kirill Tsybulka <ktsybulka11@gmail.com>
2024-07-20 18:30:53 -0400

    Part 2 complete: add bcrypt, modified validator

--- router/user_router.js
+++ router/user_router.js
@@ -1,21 +1,44 @@
+const express = require("express");
 const router = express.Router();
 const path = require("path");
 const User = require("../models/user");
-const { validateUser } = require("../middleware/validator");
+const bcrypt = require("bcrypt");
+const {
+  validateUserSignup,
+  validateUserLogin,
+} = require("../middleware/validator");
 
+// Serve static files
+router.get("/user/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views", "books.html"));
+});
 
+router.get("/user-list", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views", "books.html"));
+});
+
+// User signup
 router
   .route("/sign-up")
   .get((req, res) => {
     res.sendFile(path.join(__dirname, "../views", "sign-up.html"));
   })
-  .post(validateUser, (req, res) => {
-    const { first_name, last_name, username, email, password } = req.body;
-    const newUser = new User({first_name, last_name, username, email, password,
-    });
+  .post(validateUserSignup, (req, res) => {
+    const { first_name, last_name, username, email } = req.body;
 
-    newUser
-      .save()
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hashedPassword) => {
+        const newUser = new User({
+          first_name,
+          last_name,
+          username,
+          email,
+          password: hashedPassword,
+        });
+
+        return newUser.save();
+      })
       .then(() => {
         res.status(201).json({ message: "Successfully Added" });
       })
@@ -24,32 +47,44 @@ router
         res.status(500).json({ error: "Internal Server Error" });
       });
   });
-  router
+
+// User login
+router
   .route("/login")
-  .get((req, res) => {
+  .get(validateUserLogin, (req, res) => {
     res.sendFile(path.join(__dirname, "../views", "login.html"));
   })
   .post((req, res) => {
     const { email, password } = req.body;
-    
+
     User.findOne({ email })
-      .then(user => {
+      .then((user) => {
         if (!user) {
           console.log(`User with email ${email} not found`);
-          return res.status(400).json({ error: `User with email ${email} not found` });
+          return res
+            .status(400)
+            .json({ error: `User with email ${email} not found` });
         }
-        // Compare passwords
-        if (user.password !== password) {
-          console.log(`Incorrect password for user ${email}`);
-          return res.status(400).json({ error: `Incorrect password for user ${email}` });
-        }
-        // Passwords match, login successful
-        console.log(`Hello ${user.first_name}, with username ${user.username} (${email}) logged in successfully`);
-        res.json({ message: `Hello ${user.first_name}, with username ${user.username} (${email}) logged in successfully` });
+
+        return bcrypt.compare(password, user.password).then((isMatched) => {
+          if (!isMatched) {
+            return res
+              .status(400)
+              .json({ error: `Incorrect password for user ${email}` });
+          }
+
+          console.log(
+            `Hello ${user.first_name}, with username ${user.username} (${email}) logged in successfully`
+          );
+          res.json({
+            message: `Hello ${user.first_name}, with username ${user.username} (${email}) logged in successfully`,
+          });
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error during login:", error);
         res.status(500).json({ error: "Internal Server Error" });
       });
   });
+
 module.exports = router;
