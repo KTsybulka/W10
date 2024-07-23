03cf0f0cf0c25b4d7ca3a5ed6eb9be1bc0e4cd64
Kirill Tsybulka <ktsybulka11@gmail.com>
2024-07-22 21:40:00 -0400

    Feature: session saving

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const book_router = require("./router/book_router");
-const user_router = require('./router/user_router');
-const PORT = process.env.PORT || 8000;
-const app = express();
 require("dotenv").config();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use("/bookstore", book_router);
-app.use("/user", user_router);
+const path = require("path");
+const express = require("express");
+const session = require("express-session"); //new
+const MongoStore = require("connect-mongo"); //new
+const mongoose = require("mongoose");
 
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+// Configure session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_HOST,
+      collectionName: "sessions",
+    }),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+  })
+);
+const book_router = require("./router/book_router");
+const user_router = require("./router/user_router");
+
+// Connect to MongoDB
 mongoose.connect(process.env.DB_HOST);
 let db = mongoose.connection;
 
@@ -20,11 +35,27 @@ db.on("error", (err) => {
   console.log("DB Error:" + err);
 });
 
-app.get("/", (req, res)=> {
-  res.redirect("/bookstore");
+// Application level middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/bookstore", book_router);
+app.use("/user", user_router);
+
+app.get("/test-session", (req, res) => {
+  if (req.session.views) {
+    req.session.views++;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>Views: " + req.session.views + "</p>");
+    res.end();
+  } else {
+    req.session.views = 1;
+    res.end("Welcome to the session demo. Refresh!");
+  }
 });
 
-
+app.get("/", (req, res) => {
+  res.redirect("/bookstore");
+});
 
 app.listen(PORT, () =>
   console.log(`Server started on http://127.0.0.1:${PORT}`)
